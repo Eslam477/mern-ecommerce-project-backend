3cdd10382a4037d0c33f0d7bfc719572f6ff5166
Eslam <eslamelnemery477@gmail.com>
2023-03-21 16:33:28 +0200

    Some modifications to the server environment

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ import { Router } from "express";
 import products from "./products/index.js"
 import userRouts from './user/index.js'
 var collectionRouts = Router()
+
+collectionRouts.get('/', (req, res) => {
+    res.send('server running successfully')
+})
 collectionRouts.use(products)
 collectionRouts.use(userRouts)
 export default collectionRouts;
